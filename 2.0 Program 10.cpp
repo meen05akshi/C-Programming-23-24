b6db881d44e435b8afb10fb5584f9f71d90e2bf0
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-09-17 12:26:22 +0530

    2.0 Program 10.cpp

--- 2.0 Program 10.cpp
+++ 2.0 Program 10.cpp
@@ -11,13 +11,20 @@ int main()
 		printf("Number of days is 31");
 		
 	else if (month==4 || month==6 || month==9 || month==11)
-		printf("Number of days is 30")
+		printf("Number of days is 30");
 	
 	else if(month==2)
 		
-		if year%
+		if (year%4==0 && (year%100!=0 || year%400==0))
+			printf("Number of days is 29");
+		else
+			printf("Number of days is 28");	
 	
+	else
+		printf("Invalid input");
 	
+	return 0;
+}
 	
 	
 	return 0;
