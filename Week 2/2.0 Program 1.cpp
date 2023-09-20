de02f840a95f106066edfc2cdb77f455f2ce2c13
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-09-20 15:54:05 +0530

    Add files via upload

---
+++ Week 2/2.0 Program 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter 3 numbers: ");
+	scanf("%d %d %d",&a,&b,&c);
+	if (a>b && a>c)
+		printf("%d is the greatest",a);
+	else if (b>c && b>a)
+		printf("%d is the greatest",b);
+	else
+		printf("%d is the greatest",c);
+	return 0;
+}
