ecf03995765246cf1f53ba5e842364fa1afd89b7
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-09-23 15:36:10 +0530

    2.0 Program 1.cpp

--- Week 2/2.0 Program 1.cpp
+++ Week 2/2.0 Program 1.cpp
@@ -5,9 +5,9 @@ int main()
 	int a,b,c;
 	printf("Enter 3 numbers: ");
 	scanf("%d %d %d",&a,&b,&c);
-	if (a>b && a>c)
+	if (a>=b && a>=c)
 		printf("%d is the greatest",a);
-	else if (b>c && b>a)
+	else if (b>=c && b>=a)
 		printf("%d is the greatest",b);
 	else
 		printf("%d is the greatest",c);
