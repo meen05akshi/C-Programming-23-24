a3ca76833d7702b657cb8db78b393ce4f27b32f4
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-09-23 15:54:34 +0530

    2.0 Program 2.cpp

--- Week 2/2.0 Program 2.cpp
+++ Week 2/2.0 Program 2.cpp
@@ -4,7 +4,7 @@ int main()
 	int year;
 	printf("Enter a year:");
 	scanf("%d",&year);
-	if (year%4==0 && (year%100!=0 || year&400==0))
+	if (year%4==0 && (year%100!=0 || year%400==0))
 		printf("%d is a Leap year",year);
 	else
 		printf("%d is not a Leap year",year);
