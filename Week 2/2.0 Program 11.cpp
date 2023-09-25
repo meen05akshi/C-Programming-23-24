2ce555ed7f6a4c621f524203b52e18ef775347ae
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-09-25 12:50:44 +0530

    2.0 Program 11.cpp

--- Week 2/2.0 Program 11.cpp
+++ Week 2/2.0 Program 11.cpp
@@ -12,10 +12,9 @@ int main()
 	
 	if (month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)
 		if (date<31)
-			printf("%d",++date);
+			printf("%d %d %d",++date,month,year);
 		else if (date==31)
 		{
-			printf("1");
 			if (month==12)
 			{
 				month=1;
@@ -23,39 +22,42 @@ int main()
 			}
 			else
 				month+=1;
+			printf("1 %d %d",month,year);
 		}
 		else
-			printf("Invalid input")
+			printf("Invalid input");
 		
 	else if (month==4 || month==6 || month==9 || month==11)
 		if (date<30)
-			printf("%d",date+1);
+			printf("%d %d %d",date+1,month,year);
 		else if (date==30)
 		{
-			printf("1");
 			month+=1;
-			month+=1;
+			printf("1 %d %d",month,year);
 		}
 		else
-			printf("Invalid input")
+			printf("Invalid input");
 	
 	else if (month==2)
 		if (date<28)
-			printf("%d",date+1);
+			printf("%d %d %d",date+1,month,year);
 		else if (date==28 || date==29)
 		{	
 			if (year%4==0 && (year%100!=0 || year%400==0) && date!=29)
-				printf("29");
+				printf("29 %d %d",month,year);
+			else if (year%4!=0 && (year%100==0 || year%400!=0) && date==29)
+				printf("Invalid input");
 			else
-				printf("1");
-			month+=1;
+			{
+				month+=1;
+				printf("1 %d %d",month,year);
+			}
 		}
 		else
-			printf("Invalid input")
+			printf("Invalid input");
 	
 	else
 		printf("Invalid input");
 	
-	printf(" %d %d ",month,year);
 	return 0;
 }
