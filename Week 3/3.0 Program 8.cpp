0d09a5342977bfaf1d0ceef1ad71888a62206906
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-09-26 11:17:13 +0530

    3.0 Program 8.cpp

--- Week 3/3.0 Program 8.cpp
+++ Week 3/3.0 Program 8.cpp
@@ -4,11 +4,12 @@ int main()
 	int n,count=0;
 	printf("Enter value of n: ");
 	scanf("%d",&n);
+	int num=n;
 	while (n>0)
 	{
 		n=n/10;
 		count++;
 	}
-	printf("\n Length of %d is %d ",n,count);
+	printf("\n Length of %d is %d ",num,count);
 	return 0;
 }
