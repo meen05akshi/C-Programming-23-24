de8a0d469b9f481d703fb8dc45fda0291338ae33
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-10-29 14:27:33 +0530

    Add files via upload

---
+++ Week 3/3.0 Program 13.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Enter value of n: ");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		for(int s=0;s<n-i-1;s++)
+			printf("  "); //2 spaces		
+		
+		for(int j=0;j<2*i+1;j++)
+			printf("* "); //with space
+			
+		printf("\n");
+	}
+	
+	for(int i=0;i<n-1;i++)
+	{
+		for(int s=0;s<i+1;s++)
+			printf("  "); //2 spaces
+		
+		for(int j=0;j<2*n-2*i-3;j++)
+			printf("* "); //with space
+		
+		printf("\n");
+	}
+	return 0;
+}
