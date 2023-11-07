04dbed085a15d51b67f67fd3d959b788b76e0ac8
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-11-07 13:13:53 +0530

    Update 5.0 Program 30.cpp

--- Week 5/5.0 Program 30.cpp
+++ Week 5/5.0 Program 30.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	int n,min;
+	int n,num1=0,num2=0,sum=0,m;
 	printf("Enter size of array 1: ");
 	scanf("%d",&n);
 	int a[n];
@@ -13,14 +13,21 @@ int main()
 	scanf("%d",&m);
 	int b[m];
 	printf("Enter elements of array 2: \n");
-	for(int i=0;i<n;i++)m
+	for(int i=0;i<m;i++)
 		scanf("%d",&b[i]);
 	
-	if (n>m)
-		min=m;
-	else
-		min=n;
+	for(int i=0;i<n;i++)
+	{
+		num1=num1*10+a[i];
+	}
 	
+	for(int i=0;i<m;i++)
+	{
+		num2=num2*10+b[i];
+	}
+	
+	sum=num1+num2;
+	printf("%d ",sum);
 	
 	return 0;
 }
