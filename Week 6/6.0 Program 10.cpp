753b1385222b6d24a4676eb70350bebb76b70294
meen05akshi <143417931+meen05akshi@users.noreply.github.com>
2023-12-03 10:23:01 +0530

    Update 6.0 Program 10.cpp

--- Week 6/6.0 Program 10.cpp
+++ Week 6/6.0 Program 10.cpp
@@ -1,24 +1,35 @@
-//NOT
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 int main()
 {
+	char s[50],ch,temp;
 	int n;
-	char s[]="Hello";
-	char ch,temp;
-	printf("Enter new character: ");
+	printf("Enter a string: ");
+	scanf("%[^\n]s",s);
+	fflush(stdin);
+	printf("Enter a character: ");
 	scanf("%c",&ch);
 	printf("Enter position: ");
 	scanf("%d",&n);
-	s[n-1]=ch;
-	for(int i=n-1;i<7;i++)
+	
+	int num=strlen(s)+1;
+	
+	for(int i=0;i<num;i++)
 	{
-		temp=s[i];
-		s[i]=s[i+1];
-		s[i+1]=temp;
+		if (i==n)
+		{
+			temp=s[i];
+			s[i]=ch;
+			for(int j=num-1;j>i;j--)
+			{
+				s[j]=s[j-1];
+				if (j==i+1)
+					s[j]=temp;
+			}
+		}	
 	}
 	
-	printf("%s",s);
+	printf("Updated string: %s",s);
 	return 0;
-	
-	
 }
